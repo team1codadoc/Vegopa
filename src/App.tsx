78891e8a56799c32801c08dd0a430103555befe2
Luceta <57941164+Luceta@users.noreply.github.com>
2022-05-15 20:32:06 +0900

    💄🔧 Add: Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import RootRoute from "./routes";
+import GlobalStyles from "./styles/GlobalStyle";
 
 function App() {
-  return <RootRoute />;
+  return (
+    <>
+      <GlobalStyles />
+      <RootRoute />
+    </>
+  );
 }
 
 export default App;
