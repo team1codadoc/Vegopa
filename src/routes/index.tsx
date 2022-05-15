d2a7d93d232d116204c45396d5c214b76b9d74b5
Dongmin Ahn <eamon3481@naver.com>
2022-05-16 02:48:13 +0900

    💄 Add Select Page Style

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/main";
 import GameResult from "../pages/GameResult";
+import MbtiResultPage from "../pages/mbtiResultPage";
+import MbtiSelectPage from "../pages/mbtiSelectPage";
 
 const RootRoute = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/select/result" element={<MbtiResultPage />} />
         <Route path="/soloEat/result" element={<GameResult />} />
+        <Route path="/select/:problem" element={<MbtiSelectPage />} />
       </Routes>
     </BrowserRouter>
   );
