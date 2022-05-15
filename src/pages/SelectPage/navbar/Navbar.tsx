c4638266b6feeed3de74b2c3e6901ad53805537c
Dongmin Ahn <eamon3481@naver.com>
2022-05-16 02:48:13 +0900

    💄 Add Select Page Style

---
+++ src/pages/SelectPage/navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <h1>Selectpage</h1>;
+};
+
+export default Navbar;
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,17 +1,17 @@
 type Colors = {
   MAIN_BG: string;
   WHITE_COLOR: string;
+  LIGHT_GREY: string;
   YELLOW_COLOR: string;
   GREY_COLOR: string;
-  LIGHTGREY_COLOR: string;
 };
 
 const colors: Colors = {
   MAIN_BG: "#c98128",
   WHITE_COLOR: "#ffff",
+  LIGHT_GREY: "#f1f1f1",
   YELLOW_COLOR: "#e0a800",
   GREY_COLOR: "#B6AEAE",
-  LIGHTGREY_COLOR: "#d3d3d3",
 };
 
 export default colors;
