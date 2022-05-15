fbdb16a3c7b458f0cb0b2b7c7858fa29aaffc15c
Dongmin Ahn <eamon3481@naver.com>
2022-05-16 02:48:13 +0900

    💄 Add Select Page Style

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -5,5 +5,6 @@ import theme from "./index";
 type ThemeInterface = typeof theme;
 
 declare module "styled-components" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface DefaultTheme extends ThemeInterface {}
 }
