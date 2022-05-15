cffcf3f471f4f82523ae02eb234deaffb42a5967
Luceta <57941164+Luceta@users.noreply.github.com>
2022-05-15 20:49:13 +0900

    🌱 Fix: Update Theme settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import RootRoute from "./routes";
 import GlobalStyles from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <RootRoute />
-    </>
+    </ThemeProvider>
   );
 }
 
