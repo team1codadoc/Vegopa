f64a51d067e08498e47ae739fbd99d1121a20445
Dongmin Ahn <eamon3481@naver.com>
2022-05-16 13:37:54 +0900

    💄 Add FoodSelectPage

--- src/pages/mbtiSelectPage/mbtiSelectButton/index.tsx
+++ src/pages/mbtiSelectPage/mbtiSelectButton/index.tsx
@@ -11,7 +11,6 @@ interface props {
 
 const MbtiSelectButton = ({ text, nextPage, id, endPage }: props) => {
   const navigate = useNavigate();
-  const [click, setClick] = useState(false);
 
   const selectClickHandler = () => {
     if (nextPage === endPage) {
@@ -20,21 +19,12 @@ const MbtiSelectButton = ({ text, nextPage, id, endPage }: props) => {
       navigate(`/select/${nextPage}`);
     }
   };
-  return (
-    <SelectButton click={click} onClick={selectClickHandler}>
-      {text}
-    </SelectButton>
-  );
+  return <SelectButton onClick={selectClickHandler}>{text}</SelectButton>;
 };
 
 export default MbtiSelectButton;
 
-const SelectButton = styled.button<{ click: boolean }>`
-  a {
-    display: block;
-    width: 100%;
-    height: 100%;
-  }
+const SelectButton = styled.button`
   width: 100%;
   padding: 16px 0;
   border: 3px solid ${({ theme }) => theme.colors.GREY_COLOR};
@@ -42,6 +32,4 @@ const SelectButton = styled.button<{ click: boolean }>`
   font-size: 20px;
   margin-bottom: 77px;
   transition: all 300ms ease-in-out;
-
-  background: ${({ theme, click }) => (click ? theme.colors.GREY_COLOR : theme.colors.WHITE_COLOR)};
 `;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FoodSelectPage from "../pages/foodSelectPage";
 import Main from "../pages/main";
 import GameResult from "../pages/GameResult";
 import MbtiResultPage from "../pages/mbtiResultPage";
@@ -13,6 +14,7 @@ const RootRoute = () => {
         <Route path="/select/result" element={<MbtiResultPage />} />
         <Route path="/soloEat/result" element={<GameResult />} />
         <Route path="/select/:problem" element={<MbtiSelectPage />} />
+        <Route path="/food/:problem" element={<FoodSelectPage />} />
       </Routes>
     </BrowserRouter>
   );
